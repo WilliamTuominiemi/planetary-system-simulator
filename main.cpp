69ed95c1d2307e266e9897b078d773b8be89c483
WilliamTuominiemi <william.m.tuominiemi@gmail.com>
2025-11-09 21:59:50 +0200

    star has orbiting planet

--- main.cpp
+++ main.cpp
@@ -1,4 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <cmath>
+
+sf::Vector2f direction(sf::Vector2f from, sf::Vector2f to) {
+    float deltaX = to.x - from.x;
+    float deltaY = to.y - from.y;
+
+    float distance = std::sqrt(deltaX * deltaX + deltaY * deltaY);
+    return (1 / distance) * (from - to);
+}
+
+sf::Vector2f orbitDirection(sf::Vector2f dir) {
+    return {dir.y, -dir.x};
+}
 
 int main() {
     int windowWidth = 800;
@@ -9,9 +22,16 @@ int main() {
 
 
     float starRadius = 50.f;
+    sf::Vector2f starPosition(windowWidth / 2.f, windowHeight / 2.f);
     sf::CircleShape star(starRadius);
     star.setOrigin(starRadius, starRadius);
-    star.setPosition(windowWidth / 2.f, windowHeight / 2.f);
+    star.setPosition(starPosition);
+
+    float planetRadius = 10.f;
+    float planetOrbitSpeed = 5.f;
+    sf::Vector2f planetPosition(windowWidth / 2.f, windowHeight / 4.f);
+    sf::CircleShape planet(planetRadius);
+    planet.setOrigin(planetRadius, planetRadius);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -21,7 +41,16 @@ int main() {
         }
 
         window.clear();
+
         window.draw(star);
+
+        planet.setPosition(planetPosition); 
+        window.draw(planet);
+
+        sf::Vector2f gravity = direction(planetPosition, starPosition);
+        sf::Vector2f orbitVelocity = orbitDirection(gravity * planetOrbitSpeed);
+        planetPosition -= orbitVelocity;
+
         window.display();
     }
 
